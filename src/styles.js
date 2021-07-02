1b77646cff352711b1c200f3e0acc30e39048e1b
Sean Schmidt <schmidtsean048@gmail.com>
2021-07-02 13:50:40 -0600

    finished snake

---
+++ src/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Header = styled.h1`
+font-size: 50px;
+text-align: center;
+font-family: Snake;
+padding-top: 10%;
+color: green;
+padding-top: 5%;
+`
